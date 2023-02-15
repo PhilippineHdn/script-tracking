92e09ff78104b1202195d87b45e339ea9ce4f231
PHILIPPINE HEDIN <philippine.hedin@hotmail.fr>
2023-02-15 11:57:19 +0100

    Loop to fill hidden input

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const url = new URL(document.location);
-
 const searchParams = url.searchParams;
 
 const filter = ['utm_source','utm_medium','utm_campaign','utm_term','utm_content','src','source'];
 
-const localStorageQuery = localStorage.getItem('queryParams');
-
-const parsedlocalStorageQuery = localStorageQuery ? JSON.parse(localStorageQuery) : {};
+const queryParams = localStorage.getItem('queryParams') ? JSON.parse(localStorage.getItem('queryParams')) : {};
 
 for (const [key, value] of searchParams.entries()) {
   if (filter.includes(key)) {
-    parsedlocalStorageQuery[key] = value;
-    localStorage.setItem('queryParams', JSON.stringify(parsedlocalStorageQuery))
+    queryParams[key] = value;
+    localStorage.setItem('queryParams', JSON.stringify(queryParams))
   };
 };
+
+for (const oneParam in queryParams) {
+  if (document.getElementById(oneParam)) {
+    document.getElementById(oneParam).value = queryParams[oneParam];
+  }
+}
