79775fd7783580d2cf17a50c65e4bc36b727afc3
PHILIPPINE HEDIN <philippine.hedin@hotmail.fr>
2023-02-15 19:20:50 +0100

    Better localStorage management

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const filter = ['utm_source','utm_medium','utm_campaign','utm_term','utm_content','src','source'];
+
 const url = new URL(document.location);
 const searchParams = url.searchParams;
 
-const filter = ['utm_source','utm_medium','utm_campaign','utm_term','utm_content','src','source'];
+let queryParams = localStorage.getItem('queryParams') ? JSON.parse(localStorage.getItem('queryParams')) : {};
 
-const queryParams = localStorage.getItem('queryParams') ? JSON.parse(localStorage.getItem('queryParams')) : {};
+const searchParamsKeys = Array.from(searchParams.keys());
 
-for (const [key, value] of searchParams.entries()) {
-  if (filter.includes(key)) {
-    queryParams[key] = value;
-    localStorage.setItem('queryParams', JSON.stringify(queryParams))
+const isInFilter = searchParamsKeys.some(searchParamsKey => filter.includes(searchParamsKey));
+if (isInFilter) {
+  queryParams = {}
+  for (const [key, value] of searchParams.entries()) {
+    if (filter.includes(key)) {
+      queryParams[key] = value;
+      localStorage.setItem('queryParams', JSON.stringify(queryParams))
+    };
   };
 };
 
@@ -18,11 +24,5 @@ for (const oneParam in queryParams) {
   }
 };
 
-// Current URL: file:///Users/philippine/Sites/script-tracking/form.html?utm_medium=insta
-const nextURL = 'file:///Users/philippine/Sites/script-tracking/form.html';
-const nextTitle = 'Hidden queryParams';
-const nextState = { additionalInformation: 'Updated the URL with JS' };
-
-// This will create a new entry in the browser's history, without reloading
-window.history.pushState(nextState, nextTitle, nextURL);
-
+const nextURL = url.href.split('?')?.[0];
+window.history.pushState({}, document.title, nextURL);
