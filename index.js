485c6a81c7efaf488ccf662af25af1dfcc66d364
PHILIPPINE HEDIN <philippine.hedin@hotmail.fr>
2023-02-15 11:17:07 +0100

    filter for queryParams

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const url = new URL(document.location);
 
 const searchParams = url.searchParams;
 
+const filter = ['utm_source','utm_medium','utm_campaign','utm_term','utm_content','src','source'];
+
 const localStorageQuery = localStorage.getItem('queryParams');
+
 const parsedlocalStorageQuery = localStorageQuery ? JSON.parse(localStorageQuery) : {};
 
 for (const [key, value] of searchParams.entries()) {
-  parsedlocalStorageQuery[key] = value;
-  localStorage.setItem('queryParams', JSON.stringify(parsedlocalStorageQuery));
-}
+  if (filter.includes(key)) {
+    parsedlocalStorageQuery[key] = value;
+    localStorage.setItem('queryParams', JSON.stringify(parsedlocalStorageQuery))
+  };
+};
